468fde48845b68dfc6c5da20a796440ac9d78181
ealvarezpereira@danielcastelao.org <>
2018-01-11 13:32:35 +0100

    Ejemplo de probar libreria en java. Programacion.

---
+++ EjemploPruebaLibreria/src/ejemplopruebalibreria/EjemploPruebaLibreria.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplopruebalibreria;
+
+import rectangulo.Rectangulo;
+import triangulo.Triangulo;
+
+
+/**
+ *
+ * @author quique
+ */
+public class EjemploPruebaLibreria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Triangulo.areaTriangulo(4f,7f);
+        Rectangulo.areaRectangulo(4f,5f);
+    }
+    
+}
