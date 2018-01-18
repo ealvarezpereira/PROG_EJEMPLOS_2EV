dda9ffa4e1b1e98054ad7ac66e192fd670500dd2
ealvarezpereira@danielcastelao.org <quique@LAPTOP-2PRGJ9F7.mshome.net>
2018-01-18 13:20:09 +0100

    Ejemplo de arrays abstractas

---
+++ EjemploArraysBasico/src/ejemploarraysbasico/EjemploArraysBasico.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploarraysbasico;
+
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author quique
+ */
+public class EjemploArraysBasico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+//        float[] temperaturas = new float[7];
+//        Le damos valores elemento a elemento
+//        temperaturas[0] = 1.2f;
+//        temperaturas[1] = 2.2f;
+//        temperaturas[2] = 3.2f;
+//        temperaturas[3] = 5.2f;
+//        temperaturas[4] = 6.2f;
+//        temperaturas[5] = 7.2f;
+//        temperaturas[6] = 8.2f;
+//        Inicializacion del array
+//        float[]temperaturas = {3f,2f,5f,7f};
+
+
+        //int []numeros = {7,4,1,3};
+        //Arrays.sort(numeros);
+        //objTem.ordenarNumeros(numeros);
+        
+        
+        float[] temperaturas = new float[7];  
+        
+        for (int i = 0; i < temperaturas.length; i++) {
+
+            temperaturas[i] = Float.parseFloat(JOptionPane.showInputDialog("Introduce la temperatura " + (i + 1)));
+        }
+
+        Temperatura objTem = new Temperatura();
+
+        //objTem.mostrarArray(temperaturas);
+        //objTem.mostrarTemperatura(temperaturas);
+        //objTem.buscarElemento(temperaturas);
+
+        
+        objTem.ordenar(temperaturas);
+        Arrays.sort(temperaturas);
+        objTem.mostrarArray(temperaturas);
+    }
+
+}
---
+++ EjemploArraysBasico/src/ejemploarraysbasico/Temperatura.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploarraysbasico;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author quique
+ */
+public class Temperatura {
+
+    private int t;
+    private boolean o = false;
+    private int j = 0;
+
+    public void mostrarArray(float[] temp) {
+
+        for (int i = 0; i < temp.length; i++) {
+            System.out.println("Temperatura " + (i + 1) + " ---> " + temp[i]);
+        }
+    }
+
+    public void mostrarTemperatura(float[] temp) {
+        t = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un día determinado"));
+
+        //Validar la 
+        while (t < 1 || t > temp.length) {
+            t = Integer.parseInt(JOptionPane.showInputDialog("Numero invalido."));
+        }
+        t = t - 1;
+        System.out.println("\nTemperatura " + (t + 1) + " ---> " + temp[t]);
+    }
+
+    public void buscarElemento(float[] temp) {
+
+        //Si esta decir que posicion ocupa, y si no decir que no está en la lista.
+        float ptemp = Float.parseFloat(JOptionPane.showInputDialog("Introduzca la temperatura"));
+
+        for (int i = 0; i < temp.length; i++) {
+            if (ptemp == temp[i]) {
+                System.out.println("La temperatura fue el día " + (i + 1));
+                o = true;
+
+                break;
+            }
+        }
+
+        if (o == false) {
+            System.out.println("No hizo esa temperatura ningún día ");
+        }
+
+    }
+
+
+    public void ordenar(float[] tem) {
+        int i, j;
+        float aux;
+        for (i = 0; i < tem.length - 1; i++) {
+            for (j = i + 1; j < tem.length; j++) {
+                if (tem[i] > tem[j]) {
+                    aux = tem[i];
+                    tem[i] = tem[j];
+                    tem[j] = aux;
+                }
+            }
+        }
+
+    }
+
+}
